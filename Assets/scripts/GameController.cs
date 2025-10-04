872fcd5cb1e8746794ff2ec0f5f8992de7ac2784
DiegoHD2025 <dholgadodelgado@gmail.com>
2025-10-04 16:31:20 -0500

    Homework process

--- Assets/scripts/GameController.cs
+++ Assets/scripts/GameController.cs
@@ -3,32 +3,74 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 using TMPro;
 
-public class GameController : MonoBehaviour {
+public class GameController : MonoBehaviour
+{
+    public Player player;
+    public Ball ball;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI highscoreText;
 
-	public Player player;
-	public Ball ball;
-	public TextMeshProUGUI scoreText;
+    public GameObject gameOverCanvas; // 🟢 Nuevo: Canvas con los botones de Game Over
 
-	private float gameOverTimer = 3f;
+    private bool isGameOver = false;
 
-	// Use this for initialization
-	void Start () {
-        Debug.Log(PlayerPrefs.GetInt("scoreHigh"));
+    void Start()
+    {
+        // Mostrar el highscore almacenado
+        int highscore = PlayerPrefs.GetInt("scoreHigh", 0);
+        highscoreText.text = "Highscore: " + highscore;
+        Debug.Log("Highscore inicial: " + highscore);
+
+        // Asegúrate de ocultar el canvas al principio
+        if (gameOverCanvas != null)
+        {
+            gameOverCanvas.SetActive(false);
+        }
     }
-	
-	// Update is called once per frame
-	void Update () {
-		bool isGameOver = ball.transform.position.z < player.transform.position.z;
 
-		if (isGameOver == false) {
-			scoreText.text = "Score: " + ball.score;
-		} else {
-			scoreText.text = "Game over!\nYour final score: " + ball.score;
+    void Update()
+    {
+        // Solo verificar game over una vez
+        if (!isGameOver && ball.transform.position.z < player.transform.position.z)
+        {
+            isGameOver = true;
+            HandleGameOver();
+        }
 
-			gameOverTimer -= Time.deltaTime;
-			if (gameOverTimer <= 0f) {
-				SceneManager.LoadScene (SceneManager.GetActiveScene().name);
-			}
-		}
-	}
+        if (!isGameOver)
+        {
+            scoreText.text = "Score: " + ball.score;
+        }
+    }
+
+    void HandleGameOver()
+    {
+        scoreText.text = "Game over!\nYour final score: " + ball.score;
+
+        // Guardar el highscore si es necesario
+        int highscore = PlayerPrefs.GetInt("scoreHigh", 0);
+        if (ball.score > highscore)
+        {
+            PlayerPrefs.SetInt("scoreHigh", ball.score);
+            PlayerPrefs.Save();
+            highscoreText.text = "Highscore: " + ball.score;
+        }
+
+        // Mostrar el menú de Game Over
+        if (gameOverCanvas != null)
+        {
+            gameOverCanvas.SetActive(true);
+        }
+    }
+
+    // Estas funciones las llamas desde los botones con Gaze
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene("Practica03"); // Cambia "MainMenu" al nombre real de tu escena del menú
+    }
 }
