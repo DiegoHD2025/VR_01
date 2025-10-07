eda82692f36030fc394b36d60a757966302f1966
Jhonatan <dholgadodelgado@gmail.com>
2025-10-07 11:39:46 -0500

    CUP MEMORY GAME PROCESS

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -3,7 +3,14 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
+<<<<<<< Updated upstream
 	public float ballProximity = 4f;
+=======
+	public bool canPick = false;
+
+	public bool picked = false;
+	public bool won = false;
+>>>>>>> Stashed changes
 
 	// Use this for initialization
 	void Start () {
@@ -12,16 +19,35 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		RaycastHit hit;
+		if (canPick == true) {
 
+			if (/*GvrViewer.Instance.Triggered ||*/ Input.GetKeyDown ("space")) {
+				RaycastHit hit;
+
+				if (Physics.Raycast(transform.position, transform.forward, out hit)) {
+
+					Cup cup = hit.transform.GetComponent<Cup> ();
+					if (cup != null) {
+						canPick = false;
+
+						picked = true;
+						won = (cup.ball != null);
+
+						cup.MoveUp ();
+					}
+
+<<<<<<< Updated upstream
 		if (Physics.Raycast(transform.position, transform.forward, out hit)) {
 			if (hit.transform.GetComponent<Ball>() != null) {
 				Ball ball = hit.transform.GetComponent<Ball> ();
 
 				if (ball.transform.position.z - transform.position.z < ballProximity && ball.direction.z < 0) {
 					ball.OnPlayerHit ();
+=======
+>>>>>>> Stashed changes
 				}
 			}
+
 		}
 	}
 }
