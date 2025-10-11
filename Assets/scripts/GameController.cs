7af09faa224ba18b3291eb88f45cac147df24c35
DiegoHD2025 <dholgadodelgado@gmail.com>
2025-10-11 18:13:06 -0500

    Homework Process (Vasitos)

--- Assets/scripts/GameController.cs
+++ Assets/scripts/GameController.cs
@@ -1,80 +1,125 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using TMPro;
 
-public class GameController : MonoBehaviour {
+public class GameController : MonoBehaviour
+{
 
-	public TextMesh infoText;
-	public GameObject ball;
-	public Player player;
-	public Cup[] cups;
+    public TextMeshProUGUI infoText;
+    public TextMeshProUGUI correctCountText; // Texto para aciertos
+    public TextMeshProUGUI failCountText;    // Texto para fallos
 
-	private float resetTimer = 3f;
+    public GameObject ball;
+    public Player player;
+    public Cup[] cups;
 
-	// Use this for initialization
-	void Start () {
-		infoText.text = "Pick the correct cup!";
+    private float resetTimer = 3f;
 
-		StartCoroutine (ShuffleRoutine());
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (player.picked) {
-			if (player.won) {
-				infoText.text = "You win!";
-			} else {
-				infoText.text = "You lose :( try again!";
-			}
+    private int correctCount = 0;
+    private int failCount = 0;
+    private bool resultRegistered = false;
 
-			resetTimer -= Time.deltaTime;
-			if (resetTimer <= 0f) {
-				SceneManager.LoadScene (SceneManager.GetActiveScene().name);
-			}
-		}
-	}
+    // Use this for initialization
+    void Start()
+    {
+        correctCount = PlayerPrefs.GetInt("CorrectCount", 0);
+        failCount = PlayerPrefs.GetInt("FailCount", 0);
 
-	private IEnumerator ShuffleRoutine () {
-		yield return new WaitForSeconds (1f);
+        infoText.text = "Elige la copa correcta!";
+        UpdateScoreUI();
+        resultRegistered = false;
+        resetTimer = 3f;
+        StartCoroutine(ShuffleRoutine());
+    }
 
-		foreach (Cup cup in cups) {
-			cup.MoveUp ();
-		}
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.picked && !resultRegistered)
+        {
+            if (player.won)
+            {
+                infoText.text = "Ganaste!";
+                correctCount++;
+            }
+            else
+            {
+                infoText.text = "Perdiste :( intentalo de nuevo!";
+                failCount++;
+            }
 
-		yield return new WaitForSeconds (0.5f);
+            UpdateScoreUI();
+            resultRegistered = true; // Marca que ya se registró el resultado
+        }
 
-		Cup targetCup = cups[Random.Range(0, cups.Length)];
-		targetCup.ball = ball;
-		ball.transform.position = new Vector3 (
-			targetCup.transform.position.x,
-			ball.transform.position.y,
-			targetCup.transform.position.z
-		);
+        if (resultRegistered)
+        {
+            resetTimer -= Time.deltaTime;
+            if (resetTimer <= 0f)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
 
-		yield return new WaitForSeconds (1.0f);
 
-		foreach (Cup cup in cups) {
-			cup.MoveDown ();
-		}
+    void UpdateScoreUI()
+    {
+        correctCountText.text = "Aciertos: " + correctCount;
+        failCountText.text = "Fallos: " + failCount;
 
-		yield return new WaitForSeconds (1.0f);
+        PlayerPrefs.SetInt("CorrectCount", correctCount);
+        PlayerPrefs.SetInt("FailCount", failCount);
+        PlayerPrefs.Save();
+    }
 
-		for (int i = 0; i < 5; i++) {
-			Cup cup1 = cups[Random.Range(0, cups.Length)];
-			Cup cup2 = cup1;
+    private IEnumerator ShuffleRoutine()
+    {
+        yield return new WaitForSeconds(1f);
 
-			while (cup2 == cup1) {
-				cup2 = cups[Random.Range(0, cups.Length)];
-			}
+        foreach (Cup cup in cups)
+        {
+            cup.MoveUp();
+        }
 
-			Vector3 cup1Position = cup1.targetPosition;
+        yield return new WaitForSeconds(0.5f);
 
-			cup1.targetPosition = cup2.targetPosition;
-			cup2.targetPosition = cup1Position;
+        Cup targetCup = cups[Random.Range(0, cups.Length)];
+        targetCup.ball = ball;
+        ball.transform.position = new Vector3(
+            targetCup.transform.position.x,
+            ball.transform.position.y,
+            targetCup.transform.position.z
+        );
 
-			yield return new WaitForSeconds (0.75f);
-		}
+        yield return new WaitForSeconds(1.0f);
 
-		player.canPick = true;
-	}
+        foreach (Cup cup in cups)
+        {
+            cup.MoveDown();
+        }
+
+        yield return new WaitForSeconds(1.0f);
+
+        for (int i = 0; i < 5; i++)
+        {
+            Cup cup1 = cups[Random.Range(0, cups.Length)];
+            Cup cup2 = cup1;
+
+            while (cup2 == cup1)
+            {
+                cup2 = cups[Random.Range(0, cups.Length)];
+            }
+
+            Vector3 cup1Position = cup1.targetPosition;
+
+            cup1.targetPosition = cup2.targetPosition;
+            cup2.targetPosition = cup1Position;
+
+            yield return new WaitForSeconds(0.75f);
+        }
+
+        player.canPick = true;
+    }
 }
